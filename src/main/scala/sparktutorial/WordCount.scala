68aaddb0ce58fca65dd511e4489ae477635a07d9
Jason Baldridge <jasonbaldridge@gmail.com>
2013-04-07 23:45:50 -0500

    Fix output location.

--- src/main/scala/sparktutorial/WordCount.scala
+++ src/main/scala/sparktutorial/WordCount.scala
@@ -5,13 +5,12 @@ import SparkContext._
 
 object WordCount extends App {
 
-  println("** " + args)
-  val Array(sparkHost, sparkLocation, jar, inputfile) = args
+  val Array(sparkHost, sparkLocation, jar, inputfile, outputDir) = args
   val sc = new SparkContext(sparkHost, "WordCount", sparkLocation, Array(jar))
   //val sc = new SparkContext("local", "WordCount", "/home/jbaldrid/devel/spark", List("target/scala-2.9.2/spark-tutorial_2.9.2-0.1.jar"))
   val file = sc.textFile(inputfile)
   val counts = file.flatMap(line => line.split(" "))
     .map(word => (word, 1))
     .reduceByKey(_ + _)
-  counts.saveAsTextFile(args(1))
+  counts.saveAsTextFile(outputDir)
 }
